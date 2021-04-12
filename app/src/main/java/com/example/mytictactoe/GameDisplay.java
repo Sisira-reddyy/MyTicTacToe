36ed0c73ef41fc234dc7f6cfd2b3fe5a7d14d07a
Sisira-reddyy <reddyysisira@gmail.com>
2021-04-12 13:40:45 +0530

    Play Play

---
+++ app/src/main/java/com/example/mytictactoe/GameDisplay.java
@@ -0,0 +1,24 @@
+package com.example.mytictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_display);
+    }
+
+    public void playAgainButtonClick(View view){
+        //do fancy stuff
+    }
+    public void homeButtonClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
